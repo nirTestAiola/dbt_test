2fd1913c6ebf7f9267b4f205e0f79ce8b3a1aaa6
nir.aiola.test@gmail.com <nir.aiola.test@gmail.com>
2022-08-31 08:53:14 +0000

    b - just in case 2

---
+++ models/buchnik/dim_incremental_merge_stream_data.sql
@@ -0,0 +1,9 @@
+{{
+    config (
+        pre_hook = "SELECT * FROM {{ ref('stg_nir_buchnik_attachments') }} as stg_stream
+                    WHERE stg_stream.ITEMID not in (select ITEMID from {{ ref('stg_nir_buchnik_stream_attachments') }} as org_data
+                    WHERE EVENTNAME != 'INSERT')
+                    "
+        materialized = 'incremental'
+    )
+}}
